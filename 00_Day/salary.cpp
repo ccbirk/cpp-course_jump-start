90a3217334e27381c5d058fc2c9538e42c49acfb
ccbirk <ccbirkenstock@gmail.com>
2023-09-25 12:02:04 +0200

    salary to be ammended

---
+++ 00_Day/salary.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+using std::string;
+using std::endl;
+using std::cout;
+using std::cin;
+
+int main (){
+
+  
+
+  cout<<"********************";
+
+  string  nameSurname ;
+  cout << "Typer your name and Surname";
+  cin >> nameSurname;
+  cout<<"Name: \t\n" <<nameSurname ;
+  
+  int  x ;
+		       cout<<"What is your age?";
+			     cin >> x; 
+			     cout<<"Age " << x << "\t\n" ;
+		       
+  string  City ;
+
+			     "What is your country?" ;
+			       cin City;
+			     "City: \t\n" <<  City ;
+
+
+			     string Country;
+			     cout "What is your country?";
+    "Country: \n" << Country ;
+     cin >> Country ;
+    Cin >> x ; 
+    "Monthly Salary: \t\n" <<Salary ;
+    "Yearly Salary: \t" << Salary x 12;
+    "Gender: M \n";
+    bool married = true ;
+      bool married = false ;
+      "Married : " if (married = 1 || married = true) {
+      cout 1} ;
+ cout<<"********************";
+      
+    return 0 ;
