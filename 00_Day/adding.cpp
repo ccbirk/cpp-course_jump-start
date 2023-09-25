53ed2e4ae8c4d3794cdd59e9ae760d3161759959
ccbirk <ccbirkenstock@gmail.com>
2023-09-25 10:33:06 +0200

    adding complete

--- 00_Day/adding.cpp
+++ 00_Day/adding.cpp
@@ -6,7 +6,21 @@ using std::endl;
 
 int main() {
 
-  cout<<"hello world"<<endl;
+  string doubleEnter = {"\n\n"};
+  int n1{20} , n2{30} , n3{10} ;
+  cout<<"\n"<<n1<< " +\n";
+  cout<<n2<< " + \n";
+  cout<<n3<<doubleEnter;
 
+  cout<<"______________________________"<<doubleEnter;
+       
+  int sum = 0;
+  sum = n1 + n2 + n3;
+    cout<<"Total = \n"<<sum;
+
+
+
+
+    
   return 0;
 }
