d145ba2dc9de1850aefb5f7d9d647f58109b1fe3
ccbirk <ccbirkenstock@gmail.com>
2023-09-25 17:10:47 +0200

    Salary update

--- 00_Day/salary.cpp
+++ 00_Day/salary.cpp
@@ -11,12 +11,18 @@ int main (){
 cout<<" \n";
 string name;
 string surname;
- cout << "What is your name ?" cin >> name ;
- cout << "What is your surname?" cin >> surname;
+ cout << "What is your name ?" << cin >> name ;
+ cout << "What is your surname?" << cin >> surname;
  
- cout << "What is your age?"  >> cin Age;  
-int Age ;
-cin >> Age;
+ int Age ;
+
+ cout << "What is your age?" << cin >> Age; 
+
+ cout << "What is your salary?" ; cin >> monthlySalary ;
+
+
+
+
 string City;
 cin >> City;
 int x ;
@@ -28,7 +34,7 @@ cin >> monthlySalary ;
 int yearlySalary = monthlySalary * 12 ;
 
 cout <<"**********************\n\n" ;
-cout << "Name: " << nameSurname << ".\n";
+cout << "Name: " << name + " " << surname <<".\n";
 cout << "Age \t: " << Age <<"Years.\n";
 cout << "City\t: " << City <<".\n" ;
 cout << "Country:  " << Country << ".\n";
