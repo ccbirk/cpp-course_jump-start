245fbbed6af0ad27f5447d568ea6c305dfd9309d
ccbirk <ccbirkenstock@gmail.com>
2023-09-25 18:22:54 +0200

    new file + update

---
+++ 00_Day/years.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using std::string;
+using std::cout;
+using std::endl;
+using namespace std;
+int main (){
+
+  int sum = 0;
+
+  cout << "\n After " << '5' << " years you will be 30 years old.\n";
+
+  return 0;
+
+}
