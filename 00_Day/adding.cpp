dfef46dcbcb256d8e54aa23f33f3860cc42c4a5e
GodMode <136563161+BitByteBender@users.noreply.github.com>
2023-09-26 22:13:06 +0100

    Update adding.cpp

--- 00_Day/adding.cpp
+++ 00_Day/adding.cpp
@@ -6,17 +6,18 @@ using std::endl;
 
 int main() {
 
-  cout<<"\n" ;
-  int n1{20} , n2{30} , n3{10} ;
-  cout<<"\n"<<n1<< " +\n" ;
-  cout<<n2<< " + \n" ;
-  cout<<n3<< " + \n" ;
+  int n1{20} , n2{30} , n3{10};
+  int sum = 0;
+  sum = n1 + n2 + n3;
+  
+  cout<<n1<< " +\n";
+  cout<<n2<< " + \n";
+  cout<<n3<< " + \n";
  
 
   cout<<"______________________________"<<"\n\n";
        
-  int sum = 0;
-  sum = n1 + n2 + n3;
+
   cout<<"Total = "<< sum << "\n\n" ;
 
 
