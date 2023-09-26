1b86475bee8acf28da6511bce8d36f327beb4aeb
GodMode <136563161+BitByteBender@users.noreply.github.com>
2023-09-26 21:58:28 +0100

    Update salary.cpp

--- 00_Day/salary.cpp
+++ 00_Day/salary.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 //using namespace std;
 using std::string;
@@ -16,43 +17,53 @@ string City;
 string  Country ;
 //int monthlySalary ;
 float monthlySalary;
-string Gender;
-
- cout << " What is your name? " ;
- cin >> name ;
+//string Gender;
+char Gender;
+bool isMarried;
+ 
+cout << " What is your name? " ;
+cin >> name ;
 
 cout << " What is your surname? " ;
 cin >> surname ;
 
- cout << " What is your age? ";
- cin >> Age; 
+cout << " What is your age? ";
+cin >> Age; 
 
 cout << " What is your city's name? " ;
 cin >> City ;
 
 cout << " What is your country's name? ";
-cin >> Country ;
+//cin >> Country ;
+cin.ignore(1, '\n');
+getline(cin, country);
  
 cout << " What is your monthly salary? ";
 cin >> monthlySalary ;
 
-int yearlySalary = monthlySalary * 12 ;
-
+//int yearlySalary = monthlySalary * 12 ;
+float yearlySalary = monthlySalary * 12.0f;
+ 
 cout << " What is your gender, M/F ? " ;
 cin >> Gender ;
 
+cout<<"Are you married? ";
+cin>> isMarried;
 
 
 
 cout <<"**********************\n\n" ;
-cout << "Name\t: " << name + " " << surname <<".\n";
+//cout << "Name\t: " << name + " " << surname <<".\n";
+cout << "Name\t: " << name + " " + surname <<".\n";
+ 
 cout << "Age \t: " << Age <<" Years.\n";
 cout << "City\t: " << City <<".\n" ;
 cout << "Country :  " << Country << ".\n";
 cout << "Monthly Salary\t:" << monthlySalary << ".\n" ;
 cout << "Yearly Salary:\t" << yearlySalary << ".\n" ;
-cout << "Gender\t: M\n" ;
-cout << "Married : 1\n" ;
+//cout << "Gender\t: M\n" ;
+ cout<<"Gender:\t: "<<Gender<<".\n";
+cout << "Married\t: "<<isMarried<<"\n" ;
 
 cout<<"**********************\n" ;
 
